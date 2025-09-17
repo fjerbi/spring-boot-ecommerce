0df75accf9dc54bc3b9967ab01829fbb3b3152b9
unknown <firas.jerbi.engineer@gmail.com>
2025-09-17 11:27:12 +0200

    Generic exception handler

--- src/main/java/com/ecommerce/springcomm/controller/CategoryController.java
+++ src/main/java/com/ecommerce/springcomm/controller/CategoryController.java
@@ -8,6 +8,8 @@ import com.ecommerce.springcomm.model.Category;
 import com.ecommerce.springcomm.service.CategoryService;
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,7 +41,7 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory (@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added Successfully", HttpStatus.OK);
     }
